d440c4dc2281d3bc38ae1a345ccb8945186a21c3
jose <jennymarin1989@hotmail.com>
2018-01-18 12:02:57 +0000

    Use of arrays and methods in list of students

--- directory.rb
+++ directory.rb
@@ -1,18 +1,37 @@
-student_count = 11
 
 # first, print the list of students
-puts "The students of Villains Academy"
-puts "--------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#print final number of students
-puts "Overall we have #{11} great students"
+students = [
+  "Dr. Hannibal Lecter",
+  "Dr. Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
+# and now print them using methods:
+
+# this method will print the header
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
+end
+# this method will print the list of students
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+# this method will print the footer
+def print_footer(names)
+# finally we print the total
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
