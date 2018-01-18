4e49a8e4f6289a72c2c710d7439e06b828e901ad
jose <jennymarin1989@hotmail.com>
2018-01-18 11:31:28 +0000

    refactoring list of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+student_count = 11
+
+# first, print the list of students
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -12,7 +15,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 #print final number of students
-print "Overall we have "
-#print doesn't add a final line of characters
-print 11
-puts " great students"
+puts "Overall we have #{11} great students"
